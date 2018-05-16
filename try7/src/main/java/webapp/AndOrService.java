202f68a55eb478bedf73456e1629e26160cd6633
DavidHope <DavidHope@192.168.0.8>
2018-05-15 22:55:59 -0400

    edit

--- try7/src/main/java/webapp/AndOrService.java
+++ try7/src/main/java/webapp/AndOrService.java
@@ -4,6 +4,7 @@ public class AndOrService {
 
 	public static boolean areWordsConnected(String f1content, String f2content, String f3content) {
 		//logic for AND OR buttons
+		
 		return false;
 	}
 	
